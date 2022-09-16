fbc5eed6e1a2b99cc748503016e7be547a38ddc4
Stiefanek <107305979+Stiefanek@users.noreply.github.com>
2022-09-16 23:01:39 +0200

    Monko's

---
+++ ModHelperData.cs
@@ -0,0 +1,14 @@
+﻿namespace monko
+{
+    public static class ModHelperData
+    {
+        public const string Version = "1.1.0";
+        public const string Name = "Monko";
+        public const string Description =
+            "Adds Pat Fusty's dad Monko he's stronger than Pat Himself";
+
+        public const string RepoOwner = "stiefanek.";
+        public const string RepoName = "monko";
+        public const string Icon = "monko-Icon.png";
+    }
+}
